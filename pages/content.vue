59c322689116e8ff2dc7860e50130349934d15db
minternational <mintkuehn@gmail.com>
2022-07-12 14:27:42 +0200

    add navigation and default page

---
+++ pages/content.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Here is the content page for all blog articles</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Hello World!
+    {{ count }}
+    <button @click="increment">Increment</button>
+    <a href="/content">See Content</a>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: "default",
+  });
+
+  const count = ref(0);
+  const increment = () => count.value++
+</script>
